cde676ffca3f5804a8af18c067714e1785313bed
Syed Faheem <syedfaheem2719@gmail.com>
2024-06-16 18:59:09 +0530

    Handle form submission and log the entries on the console

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public handleSubmit(event: Event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    //add types
+    const username = formData.get('username');
+    const password = formData.get('username');
+
+    console.log(username, password);
   }
-
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
   constructor() {}
 
   ngOnInit(): void {}
 
+  public handleSubmit(event: Event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const username = formData.get('username');
+    const emailId = formData.get('email');
+    const phoneNumber = formData.get('phone');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirmPassword');
+
+    console.log(username, emailId, password, confirmPassword, phoneNumber);
+  }
 }
