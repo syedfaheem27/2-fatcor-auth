1fb88a06800d3d022a245fcee53519177c51a9c9
Syed Faheem <syedfaheem2719@gmail.com>
2024-06-22 00:45:13 +0530

    Complete signup component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.interface';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,21 +9,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  private api_url = "https://localhost:44339/api/User/register";
 
-  ngOnInit(): void {}
+  constructor(private router: Router) { }
 
-  public handleSubmit(event: Event) {
+  ngOnInit(): void { }
+
+  public async handleSubmit(event: Event) {
     event.preventDefault();
 
     const formData = new FormData(event.target as HTMLFormElement);
 
-    const username = formData.get('username');
-    const emailId = formData.get('email');
-    const phoneNumber = formData.get('phone');
-    const password = formData.get('password');
-    const confirmPassword = formData.get('confirmPassword');
+    const username = formData.get('username') as string;
+    const emailId = formData.get('email') as string;
+    const phoneNumber = formData.get('phone') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
 
-    console.log(username, emailId, password, confirmPassword, phoneNumber);
+    if (!username)
+      return alert("Username is missing!");
+
+    if (!emailId && !phoneNumber)
+      return alert("EmailId and phone number are missing!");
+
+    if (!password)
+      return alert("Password is missing!");
+
+    if (password !== confirmPassword)
+      return alert("Password and confirm password are not equal!");
+
+
+    let user: User;
+
+    if (!emailId)
+      user = {
+        username,
+        role: "User",
+        password,
+        phone: phoneNumber
+      }
+
+    if (!phoneNumber)
+      user = {
+        username,
+        role: "User",
+        password,
+        emailId
+      }
+    try {
+      const res = await fetch(this.api_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify(user!)
+      });
+
+
+      const data = await res.json();
+
+      if (data.isSuccessful)
+        alert("User successfully reg◘istered!");
+      else {
+        alert(data.message);
+        throw new Error(data.message);
+      }
+
+      this.router.navigate(['/login']);
+
+    } catch (err) {
+      console.log(err, "error");
+    }
+
   }
 }
---
+++ src/app/models/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+    role?: string;
+    username: string;
+    emailId?: string;
+    phone?: string;
+    password: string
+
+}
